87ec79e1eac8e8e8e57d083d593f61885ed59d4e
Michael Salter <salterm@users.noreply.github.com>
2016-06-28 11:54:25 -0700

    Added basic code and naive input/output methods.

--- src/PicrossDetective.java
+++ src/PicrossDetective.java
@@ -1,8 +1,104 @@
 /**
- * Created by m2sal on 6/24/2016.
+ * Created by Michael Salter on 6/24/2016.
  */
+ 
+import java.util.Scanner;
+ 
 public class PicrossDetective {
+    public final int height;
+    public final int width;
+    
+    private final int maxHeight = 10;
+    private final int maxWidth = 10;
+    
+    private class Clues {
+        private Vector<int>[] rows;
+        private Vector<int>[] columns;
+    }
+    
+    private enum Colors {VOID, WHITE, BLACK};
+    
+    private class Puzzle {
+        private Colors[][] puzzle; 
+        
+        private void fill(int row, int column) {
+            
+        }
+        
+        private void print() {
+            StringBuilder sb = new StringBuilder();
+            for (Colors[] ca; puzzle) {
+                for (Colors c; ca) {
+                    switch (c) {
+                        case VOID {
+                            sb.append('?');
+                            break;
+                        }
+                        case WHITE {
+                            sb.append('0');
+                            break;
+                        }
+                        case BLACK {
+                            sb.append('#');
+                            break;
+                        }
+                    }
+                }
+                sb.append('/n');
+            }
+            
+            System.out.println(sb);
+        }
+    }
+    
+    private Clues clues;
+    private Puzzle puzzle;
+    
     void main () {
         
+        Scanner in = new Scanner(System.in); 
+        
+        //Get height of puzzle
+        int input = 0;
+        while(input == 0) {
+            System.out.println("Enter input: ");
+        
+            input = in.nextInt();
+            if (input <= 0 || input > maxHeight) {
+                System.out.println("Invalid number.");
+                input = 0;
+            }
+        }
+        
+        height = input;
+        
+        //Get width of puzzle
+        input = 0;
+        while(input == 0) {
+            System.out.println("Enter input: ");
+        
+            input = in.nextInt();
+            if (input <= 0 || input > maxWidth) {
+                System.out.println("Invalid number.");
+                input = 0;
+            }
+        }
+        
+        width = input;
+        
+        //Get row clues
+        
+        //Get column clues
+        
+        //Begin solving
+        boolean solved = false;
+        while (!cluesSatisfied()) {
+            //solve
+            
+        }
+    }
+    
+    boolean cluesSatisfied() {
+        return false;
     }
 }
